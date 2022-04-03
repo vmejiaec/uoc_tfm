9b5094bea24e5fecbdd9c9daa3fb4fc95fed14bb
vmejiaec <vmejiaec@gmail.com>
2022-04-03 14:08:37 -0500

    Los caminos a los dos almacenes se controlan medianta la ia de Citizens2 y se regulan con un reloj

--- citnpc01/src/main/java/vmejiaec/com/citnpc/MyTrait.java
+++ citnpc01/src/main/java/vmejiaec/com/citnpc/MyTrait.java
@@ -23,22 +23,24 @@ public class MyTrait extends Trait {
 
     // El mundo
     World world = null;
-    // La localización de las bases
-    Location locbase1 = null;
+    // La position de las bases
+
     Vector vecbase1 = new Vector(151, 4, -105);
-    // La localización de los almacenes
-    Location locAlmacen1 = null;
-    Vector vecAlmacen1 = new Vector(173, 4, -103);
-    Location locAlmacen2 = null;
+    Vector vecbase2 = new Vector(156, 4, -86);
+
+    // La posición de los almacenes
+
+    Vector vecAlmacen1 = new Vector(172, 4, -103);
+    Vector vecAlmacen2 = new Vector(172, 4, -90);
+
     // La localización de los puntos de los caminos
     //  -- Camino 1 al Almacen 1
-    Vector C1v1 = new Vector(151, 4, -105);
+    Vector C1v1 = new Vector(152, 4, -105);
     Vector C1v2 = new Vector(156, 4, -103);
     Vector C1v3 = new Vector(161, 4, -103);
     Vector C1v4 = new Vector(161, 4, -107);
     Vector C1v5 = new Vector(169, 4, -107);
-    Vector C1v6 = new Vector(173, 4, -103);
-
+    Vector C1v6 = new Vector(172, 4, -90);
     List<Vector> caminoAlm1 = new ArrayList<Vector>(){
         {add(C1v1);add(C1v2);add(C1v3);add(C1v4);add(C1v5);add(C1v6);}};
 
@@ -81,19 +83,49 @@ public class MyTrait extends Trait {
         System.out.println("  -- <[ Evento de navegación BEGIN");
     }
 
-    @EventHandler
-    public void navigationbegin( NavigationCompleteEvent event){
-        System.out.println("  -- ]> Evento de navegación COMPLETE");
+    int posactual = 0;
+
+    public  void movetonextpos(){
+        System.out.print(" -- movetonextpos: "+posactual);
+        if (posactual == 0 ) posactual = 5 ; else posactual = 0;
+        System.out.print(" -- movetonextpos: "+posactual);
+        npc.getNavigator().setTarget(new Location(
+                world,
+                caminoAlm1.get(posactual).getX(),
+                caminoAlm1.get(posactual).getY(),
+                caminoAlm1.get(posactual).getZ()
+        ));
     }
 
     @EventHandler
-    public void navigationbegin( NavigationCancelEvent event){
-        System.out.println("  -- (X) Evento de navegación CANCEL");
+    public void navigationcomplete( NavigationCompleteEvent event){
+        System.out.println("  -- ]> Evento de navegación COMPLETE");
+        // Confirma que ha llegado al destino
+        Location locnpc = npc.getStoredLocation();
+        double distancia =  caminoAlm1.get(posactual).distance(npc.getStoredLocation().toVector())  ;
+        System.out.println(" -- Distancia del Npc al destino: "+distancia);
+        if (distancia <= 3 ){ // llegó al destino
+            trigerbeginmove = true;
+        } else {              // aun no llega al destino
+            if (posactual == 0 ) posactual = 5 ; else posactual = 0;
+            trigerbeginmove = true;
+        }
     }
+
+    @EventHandler
+    public void navigationcancel( NavigationCancelEvent event){
+        System.out.println("  -- (X) Evento de navegación CANCEL");
+        System.out.println("  -- (X) -- Reason: "+event.getCancelReason().toString());
+        System.out.println("  -- (X) -- Reason name: "+event.getCancelReason().name());
+        trigerbeginmove = true;
+    }
+
+
 
     int n_tick = 0;
-    int n_tick_max = 100;
-    int posactual = 0;
+    int n_tick_max = 50;
+
+    boolean trigerbeginmove = true;
 
     // Called every tick
     @Override
@@ -104,26 +136,12 @@ public class MyTrait extends Trait {
             System.out.println("TICK + "+n_tick);
             System.out.println(" -- Está navegando?: "+npc.getNavigator().isNavigating() );
             n_tick = 0;
-            // Se pone a mover al NPC
-            if (posactual == 0 ) posactual = 5 ; else posactual = 0;
 
-            npc.getNavigator().setTarget(new Location(
-                    world,
-                    caminoAlm1.get(posactual).getX(),
-                    caminoAlm1.get(posactual).getY(),
-                    caminoAlm1.get(posactual).getZ()
-            ));
-
-
-
-            Location locnpc = npc.getStoredLocation();
-
-            // Cálculo de la distancia
-
-            double d2 =  vecbase1.distance(npc.getStoredLocation().toVector())  ;
-
-            System.out.println(" -- Distancia del Npc a la base1: "+d2);
-
+            // Si no ha empezado el viaje, lo inicia
+            if(trigerbeginmove) {
+                trigerbeginmove = false;
+                movetonextpos();
+            }
 
         }
 
